cb1c3a67db2ea11bd24f5d4a43cc48ed96b0aedd
15331011 <540205863@qq.com>
2020-11-27 17:09:16 +0800

    尝试vue的原生gzip是否能不通过后端直接返回

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
-// const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const resolve = dir => {
   return path.join(__dirname, dir);
